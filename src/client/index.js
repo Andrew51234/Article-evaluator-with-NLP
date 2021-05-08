abb0b34905fedaa0b3352d0c655cfc2f5da5b0e6
Andrew Hany <andrewdodz51234@gmail.com>
2021-05-08 12:30:31 +0200

    done

--- src/client/index.js
+++ src/client/index.js
@@ -1,12 +1,14 @@
 import { checkURL } from "./js/urlCheck"
 import { handleSubmit } from "./js/formHandler"
 
+//importing scss files
 import './styles/base.scss'
 import './styles/footer.scss'
 import './styles/form.scss'
 import './styles/header.scss'
 import './styles/resets.scss'
 
+//implementing the submit button's functionality
 window.addEventListener('DOMContentLoaded', () => {
     const submit = document.getElementById('btn-submit');
     submit.addEventListener('click', (e) => {
